2119f83940516520ca5fc59a2615f5356d8e2c68
Jun Xu <jxu@lendingclub.com>
2015-07-02 23:46:32 -0700

    add read4

--- judge/java/src.java
+++ judge/java/src.java
@@ -4,10 +4,10 @@ import java.lang.*;
 import java.io.*;
 import tests.test_common;
 import datastruct.*;
-import tests.bst_to_dlist;
+import tests.48;
 
 public class src {
     public static void main (String[] args) throws java.lang.Exception {
-        bst_to_dlist.judge();
+        48.judge();
     }
 }
