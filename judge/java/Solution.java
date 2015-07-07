72d86d6ccb009b9c287226c64c2c535242d0aff5
Jun Xu <jxu@lendingclub.com>
2015-07-06 23:32:25 -0700

    add Read N Characters Given Read4 II - Call multiple times

--- judge/java/Solution.java
+++ judge/java/Solution.java
@@ -1,28 +1,28 @@
-package judge;import java.util.*;import java.lang.*;import java.io.*;import datastruct.*; /*
-public class TreeNode {
-    public int val;
-    public TreeNode left, right;
-    public TreeNode(int x) { val = x; }
-}
-*/
-public class Solution {
-    public TreeNode bst_to_list(TreeNode root) {
-        if (root == null) return null;
-        TreeNode[] head = new TreeNode[1]; // tricky
-        TreeNode[] prev = new TreeNode[1];
-        convert(root, head, prev);
-        return head[0];
-    }
-    
-    private void convert(TreeNode node, TreeNode[] head, TreeNode[] prev) {
-        if (node == null) return;
-        convert(node.left, head, prev);
-        if (prev[0] == null) head[0] = node;
-        else {
-            node.left = prev[0];
-            prev[0].right = node;
+package judge;import java.util.*;import java.lang.*;import java.io.*;import datastruct.*; public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        int readCount = 0;
+        char[] buf4 = new char[4];
+        
+        int count = 0;
+        while(count < n){
+            int read4Count = read4(buf4);
+            
+            for(int i = 0; i < Math.min(read4Count, n - readCount); i++){
+                buf[readCount + i] = buf4[i];
+            }
+            
+            readCount += Math.min(read4Count, n - readCount);
+            
+            if(read4Count < 4){
+                break;
+            }
         }
-        prev[0] = node; //
-        convert(node.right, head, prev);
+        
+        return count;
     }
 }
