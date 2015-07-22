2bb1abc1d54ea4d58314d5ca4eaadb2b440fcc1e
Jun Xu <jxu@lendingclub.com>
2015-07-21 23:41:34 -0700

    solve nested iterator

--- judge/java/Solution.java
+++ judge/java/Solution.java
@@ -17,6 +17,7 @@ package judge;import java.util.*;import java.lang.*;import java.io.*;import data
     }
 }
 
+//http://codereview.stackexchange.com/questions/32827/flatten-iterator-for-nested-list
 public class NestedIterator<NestedListNode> implements Iterator<?>{
     
     private Stack<Iterator> stack = null;
@@ -34,5 +35,55 @@ public class NestedIterator<NestedListNode> implements Iterator<?>{
     
     public NestedListNode next(){
         
+    }
+}
+
+//http://baozitraining.org/blog/design-and-implement-a-deep-iterator/
+public class DeepIterator implements Iterator {
+    private Stack iteratorStack = new Stack();
+    private Integer top = null;
+
+    public DeepIterator(Iterable iterable){
+        this.iteratorStack.push(iterable.iterator());
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(this.top != null) return true;
+
+        while(!this.iteratorStack.isEmpty()) {
+            Iterator tmpIterator = this.iteratorStack.peek();
+
+            if(tmpIterator.hasNext()){
+                Object tmp = tmpIterator.next();
+                if(tmp instanceof Integer){
+                    this.top = (Integer) tmp;
+                    return true;
+                } else if(tmp instanceof Iterable){
+                    this.iteratorStack.push(((Iterable) tmp).iterator());
+                } else {
+                    throw new RuntimeException("Unsupported data type. ");
+                }
+            } else {
+                this.iteratorStack.pop();
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() throws NoSuchElementException {
+        if(hasNext()){
+            Integer tmp = this.top;
+            this.top = null;
+            return tmp;
+        }
+
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("This is not supported right now.");
     }
 }
