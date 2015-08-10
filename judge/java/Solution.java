07f6476fb66a3a6e9c5db86286cb3fe91becd22a
Jun Xu <jxu@lendingclub.com>
2015-08-09 23:04:07 -0700

    solve uniform tree

--- judge/java/Solution.java
+++ judge/java/Solution.java
@@ -1,89 +1,18 @@
-package judge;import java.util.*;import java.lang.*;import java.io.*;import datastruct.*; public class NestedList {
- 
-    public static List<Object> flattenList(List<Object> inList) {
-        List<Object> newList = new LinkedList<Object>();
-
-        for (Object i : inList) {
-                // If it's not a list, just add it to the return list.
-                if (!(i instanceof List)) {
-                        newList.add(i);
-                } else {
-                        // It's a list, so add each item to the return list.
-                        newList.addAll(flattenList((List<Object>)i));
-                }
+package judge;import java.util.*;import java.lang.*;import java.io.*;import datastruct.*; public class Solution{
+    private int count = 0;
+    
+    public boolean validUniformTree(TreeNode root){
+        if(root == null){
+            return true;
         }
-
-        return newList;
-    }
-}
-
-//http://codereview.stackexchange.com/questions/32827/flatten-iterator-for-nested-list
-public class NestedIterator<NestedListNode> implements Iterator<?>{
-    
-    private Stack<Iterator> stack = null;
-    
-    public NestedIterator(){
-        if(in != null){
-            stack = new Stack<Iterator>();
-            stack.push(in.iterator);
-        }
-    }
-    
-    public boolean hasNext(){
-        
-    }
-    
-    public NestedListNode next(){
-        
-    }
-}
-
-//http://baozitraining.org/blog/design-and-implement-a-deep-iterator/
-public class DeepIterator implements Iterator {
-    private Stack iteratorStack = new Stack();
-    private Integer top = null;
-
-    public DeepIterator(Iterable iterable){
-        this.iteratorStack.push(iterable.iterator());
-    }
-
-    @Override
-    public boolean hasNext() {
-        if(this.top != null) return true;
-
-        while(!this.iteratorStack.isEmpty()) {
-            Iterator tmpIterator = this.iteratorStack.peek();
-
-            if(tmpIterator.hasNext()){
-                Object tmp = tmpIterator.next();
-                if(tmp instanceof Integer){
-                    this.top = (Integer) tmp;
-                    return true;
-                } else if(tmp instanceof Iterable){
-                    this.iteratorStack.push(((Iterable) tmp).iterator());
-                } else {
-                    throw new RuntimeException("Unsupported data type. ");
-                }
-            } else {
-                this.iteratorStack.pop();
+        boolean left = validUniformTree(root.left);
+        boolean right = validUniformTree(root.right);
+        if(left && right){
+            if((root.left == null || root.left.val == root.val) && (root.right == null || root.right.val == root.val)){
+                count++;
+                return true;
             }
         }
         return false;
-    }
-
-    @Override
-    public Integer next() throws NoSuchElementException {
-        if(hasNext()){
-            Integer tmp = this.top;
-            this.top = null;
-            return tmp;
-        }
-
-        throw new NoSuchElementException();
-    }
-
-    @Override
-    public void remove() {
-        throw new UnsupportedOperationException("This is not supported right now.");
     }
 }
--- judge/java/src.java
+++ judge/java/src.java
@@ -4,10 +4,10 @@ import java.lang.*;
 import java.io.*;
 import tests.test_common;
 import datastruct.*;
-import tests.nested_iterator;
+import tests.uniform_tree;
 
 public class src {
     public static void main (String[] args) throws java.lang.Exception {
-        nested_iterator.judge();
+        uniform_tree.judge();
     }
 }
