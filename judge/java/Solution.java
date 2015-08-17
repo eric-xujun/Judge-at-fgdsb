efc6bf4e2f9f9a4ee21d5a44338332ae05df592a
Jun Xu <jxu@lendingclub.com>
2015-08-16 21:31:22 -0700

    add array jump

--- judge/java/Solution.java
+++ judge/java/Solution.java
@@ -1,67 +1,36 @@
-package judge;import java.util.*;import java.lang.*;import java.io.*;import datastruct.*; // This is the text editor interface
-// Anything you type or change here will be seen by the other person in real time
-// An "expression" is either a number, or a list (AKA vector/array) with the first element one of '+', '-', '*', '/' ,
-// and then other expressions.
-// Write a function "evaluate" that gets an expression and returns its value.
-// ['+', 1, 2] --> 3
-// ['*', 3, ['+', 1, 10]] --> 33
-// ['+', 1000, 1200] --> 2200
-// ['/', 6, ['+', 1, ['*', 1, 1]]] --> 3
-// ['+', 1, 2, 3] --> 63
-// ['+', ['*', 1, 2], 3] --> 5
-// ['-', ['*', 5, 5], ['*', 2, 2]] --> 21
-// Assume - / -- only take two operands
-// + * take any number of operands
-// ['*', 0, 5] --> 0
-// ['-', 2, 1] --> 1
-public class EvaluationExpression {
-    public int valueOfExpression(ENode root) {
-        if(root == null)        
-            return 0;
-        switch(root.op){
-            case '+': {
-                int val = 0;
-                for(ENode nd : root.child) {
-                        val += valueOfExpression(nd);
-                }
-                return val;
-            }
-            case '-': {
-                int val = (root.child.size() == 0) ? 0 : valueOfExpression(root.child.get(0));                                ) f. {- _# u- F% q
-                for(int i = 1; i < root.child.size(); ++i) {
-                        val -= valueOfExpression(root.child.get(i));
-                }
-                return val;
-            }
-            case '*': {
-                int val = 1;
-                for(ENode nd : root.child) {
-                        val *= valueOfExpression(nd);
-                }
-                return val;
-            }
-            case '/': {
-                int val = (root.child.size() == 0) ? 0 : valueOfExpression(root.child.get(0));
-                for(int i = 1; i < root.child.size(); ++i) {
-                        val /= valueOfExpression(root.child.get(i));
-                }
-                return val;
-            }
-            default: return root.val;
-        }                 
+package judge;import java.util.*;import java.lang.*;import java.io.*;import datastruct.*; public class Solution{
+    public boolean canWin(int[] array, int start){
+        if(array == null || array.length == 0 || start < 0 || start >= array.length){
+            return false;
+        }
+        
+        Map<Integer> visited = new HashMap<Integer>();
+        return helper(array, start, visited);
     }
-} 
-
-class ENode {
-    char op;
-    int val;
-    List<ENode> child = new ArrayList<ENode>();
-    public ENode(int val) {
-        op = ' ';
-        this.val = val;
-    }
-    public ENode(char op) {
-        this.op = op;
-        val = 0;
+    
+    public boolean helper(int[] array, int start, Map<Integer> visited){
+        if(array[start] == 0){
+            true;
+        }
+        
+        visited.put(start, false);
+        boolean canWin = false;
+        if(start - array[start] >= 0){
+            if(visited.contains(start - array[start])){
+                canWin = canWin || visited.get(start - array[start]);
+            }else{
+                canWin = canWin || helper(array, start - array[start], visited);
+            }
+        }
+        
+        if(start + array[start] < array.length){
+            if(visited.contains(start + array[start])){
+                canWin = canWin || visited.get(start + array[start]);
+            }else{
+                canWin = canWin || helper(array, start + array[start], visited);
+            }
+        }
+        
+        return canWin;
     }
 }
--- judge/java/src.java
+++ judge/java/src.java
@@ -4,10 +4,10 @@ import java.lang.*;
 import java.io.*;
 import tests.test_common;
 import datastruct.*;
-import tests.evaluate_expressions;
+import tests.array_jump;
 
 public class src {
     public static void main (String[] args) throws java.lang.Exception {
-        evaluate_expressions.judge();
+        array_jump.judge();
     }
 }
