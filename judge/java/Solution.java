daea68fbc3ea199953d0129005af7c44edaf61d8
Jun Xu <jxu@lendingclub.com>
2015-08-18 21:39:31 -0700

    add n queen threat

--- judge/java/Solution.java
+++ judge/java/Solution.java
@@ -1,53 +1,45 @@
-package judge;import java.util.*;import java.lang.*;import java.io.*;import datastruct.*; public class Solution{
-    public static int findLongestChain(String[] words){
-        if(null == words || 0 == words.length){
-                return 0;
-        }
-        //把所有的不同的字符长度都求出来，然后从小到大排好序
-        HashSet<Integer> lengthSet = new HashSet<Integer>();
-        HashMap<Integer, HashSet<String>> stringMap = new HashMap<Integer, HashSet<String>>();
-        HashMap<String, Integer> lengthMap = new HashMap<String, Integer>();
-        int max = 1;
-        for(int i = 0; i < words.length; i++){
-            if(null != words[i]){
-                lengthSet.add(words[i].length());
-                lengthMap.put(words[i], 1);
-                if(!stringMap.containsKey(words[i].length())){
-                    stringMap.put(words[i].length(), new HashSet<String>());
-                }
-                HashSet<String> stringSet = stringMap.get(words[i].length());
-                stringSet.add(words[i]);
-            }                        
-        }
+package judge;import java.util.*;import java.lang.*;import java.io.*;import datastruct.*; public class N_Queen {
+        static int[] nx = {1, 1};
+        static int[] ny = {1, -1};
         
-        int[] lengths = new int[lengthSet.size()];
-        Iterator<Integer> lenIter = lengthSet.iterator();
-        int j = 0;
-        while(lenIter.hasNext()){
-                lengths[j++] = lenIter.next();
-        }
-        Arrays.sort(lengths);
-        
-        //从长度最短的字符串开始找
-        for(int i = 0; i < lengths.length; i++){
-            HashSet<String> stringSet = stringMap.get(lengths[i]);
-            HashSet<String> lessStringSet = stringMap.get(lengths[i] - 1);
-            if(null != lessStringSet){
-                Iterator<String> iter = stringSet.iterator();
-                while(iter.hasNext()){
-                    String str = iter.next();
-                    for(int k = 0; k < str.length(); k++){
-                        String newStr = str.substring(0, k) + str.substring(k + 1);
-                        if(lessStringSet.contains(newStr)){
-                            int maxLen = Math.max(lengthMap.get(newStr) + 1, lengthMap.get(str)); 
-                            lengthMap.put(str, maxLen);
-                            max = Math.max(maxLen, max);
+        //只有当前扫描的位置上有queen的时候才返回，否则一旦越界了，就返回-1，代表这个queen不受任何威胁
+        public static int findThreatLine(int[] pos, int x, int y, int mov1, int mov2){
+                x += mov1;
+                y += mov2;
+                while(y >= 0 && x < pos.length){
+                        if(pos[x] == y){
+                                return x;
                         }
-                    }
+                        x += mov1;
+                        y += mov2;
                 }
-            }
+                return -1;
         }
-        //In case that all of the strings are null
-        return lengthSet.size() == 0 ? 0 : max;
-    }
+        
+        public static int findMaxThreat(int[] pos){
+                if(null == pos || 0 == pos.length){
+                        return 0;
+                }
+                //由于每一行每一列都只有一个queen，所以这里的map里面，只用行号来代表当前行上的那个queen了
+                HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+                int max = 0;
+                for(int i = 0; i < pos.length; i++){
+                        map.put(i, 0);
+                }
+                for(int i = 0; i < pos.length; i++){
+                        int threatLine1 = findThreatLine(pos, i, pos[i], nx[0], ny[0]);
+                        int threatLine2 = findThreatLine(pos, i, pos[i], nx[1], ny[1]);
+                        if(threatLine1 != -1){
+                                map.put(i, map.get(i) + 1);
+                                map.put(threatLine1, map.get(threatLine1) + 1);
+                        }
+                        if(threatLine2 != -1){
+                                map.put(i, map.get(i) + 1);
+                                map.put(threatLine2, map.get(threatLine2) + 1);
+                        }
+                        max = Math.max(max, map.get(i));
+                }
+                return max;
+        }
+
 }
--- judge/java/src.java
+++ judge/java/src.java
@@ -4,10 +4,10 @@ import java.lang.*;
 import java.io.*;
 import tests.test_common;
 import datastruct.*;
-import tests.longest_chain;
+import tests.n_queen_threat;
 
 public class src {
     public static void main (String[] args) throws java.lang.Exception {
-        longest_chain.judge();
+        n_queen_threat.judge();
     }
 }
